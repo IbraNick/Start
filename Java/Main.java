f8c14c138185ea9fc88af7fe6c8dffa226296128
Ibrahim Nickel <ibrahimnickel@gmail.com>
2025-10-29 10:42:55 +0100

    oct.commit

---
+++ Java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I like Pizza \nIts really good");
+        System.out.println("Buy me Pizza");
+    }
+}
